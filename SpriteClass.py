8935d2bec20f35ff8b0f02f448651cdee3c6e875
Nysa11 <nysakeuk124@gmail.com>
2024-10-19 09:47:07 +0700

    Update SpriteClass.py

--- SpriteClass.py
+++ SpriteClass.py
@@ -1,9 +1,9 @@
 from pygame import *
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_x, player_y, player_speed, wight, height):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
         super().__init__()
-        self.image = transform.scale(image.load(player_image), (wight, height)) #e.g. 55,55 - parameters
+        self.image = transform.scale(image.load(player_image), (width, height))  # Adjust width and height parameters
         self.speed = player_speed
         self.rect = self.image.get_rect()
         self.rect.x = player_x
@@ -17,11 +17,12 @@ class Player(GameSprite):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 500 - 80:
+        if keys[K_DOWN] and self.rect.y < 500 - self.rect.height:
             self.rect.y += self.speed
+            
     def update_left(self):
         keys = key.get_pressed()
         if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_s] and self.rect.y < 500 - 80:
+        if keys[K_s] and self.rect.y < 500 - self.rect.height:
             self.rect.y += self.speed
